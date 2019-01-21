7badc478f997257f18c835cf771c94e32f35ce3b
Micpol <webowy.mail@gmail.com>
2019-01-21 08:45:01 +0100

    Added change language dialog and its components

---
+++ library/src/main/java/pl/netigen/language/ChangeLanguageParams.java
@@ -0,0 +1,35 @@
+package pl.netigen.language;
+
+import android.support.annotation.StringRes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.netigen.netigenapi.R;
+
+public class ChangeLanguageParams {
+    @StringRes
+    public int titleResId = R.string.change_language;
+    @StringRes
+    public int negativeButtonResId = R.string.cancel_upper_case;
+    @StringRes
+    public int positiveButtonResId = R.string.ok_upper_case;
+    String jsonLanguageCodes;
+    List<String> languageCodes;
+    ChangeLanguageDialogFragment.LanguageClickListener languageClickListener;
+
+    public ChangeLanguageParams() {
+    }
+
+    public ChangeLanguageParams(int titleResId, int negativeButtonResId, int positiveButtonResId,
+                                String jsonLanguageCodes, List<String> languageCodes,
+                                ChangeLanguageDialogFragment.LanguageClickListener languageClickListener) {
+        if (titleResId != 0) this.titleResId = titleResId;
+        if (negativeButtonResId != 0) this.negativeButtonResId = negativeButtonResId;
+        if (positiveButtonResId != 0) this.positiveButtonResId = positiveButtonResId;
+        this.titleResId = titleResId;
+        this.jsonLanguageCodes = jsonLanguageCodes;
+        this.languageCodes = languageCodes;
+        this.languageClickListener = languageClickListener;
+    }
+}
