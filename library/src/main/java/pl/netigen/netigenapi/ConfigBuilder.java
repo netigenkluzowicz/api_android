de18236d822b1e72725d95e9e7aa4f418ef60eed
Micpol <webowy.mail@gmail.com>
2019-01-24 14:19:45 +0100

    Added Config builder methods to set test devices

--- library/src/main/java/pl/netigen/netigenapi/ConfigBuilder.java
+++ library/src/main/java/pl/netigen/netigenapi/ConfigBuilder.java
@@ -1,8 +1,15 @@
 package pl.netigen.netigenapi;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfigBuilder {
+
     private boolean inDebugMode;
     private boolean isSamsung;
+    private List<String> testDevices;
 
     public ConfigBuilder setInDebugMode(boolean inDebugMode) {
         this.inDebugMode = inDebugMode;
@@ -14,7 +21,20 @@ public class ConfigBuilder {
         return this;
     }
 
+    public ConfigBuilder setTestDevices(List<String> testDevices) {
+        this.testDevices = testDevices;
+        return this;
+    }
+
+    public ConfigBuilder addTestDevice(String testDevice) {
+        if (this.testDevices == null) testDevices = new ArrayList<>();
+        this.testDevices.add(testDevice);
+        return this;
+    }
+
     public Config createConfig() {
-        return new Config(inDebugMode, isSamsung);
+        Config config = new Config(inDebugMode, isSamsung);
+        config.setTestDevices(this.testDevices);
+        return config;
     }
 }
