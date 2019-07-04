7cf6a43c6391923a1829581f492bab8bea253fe5
m.bogucki <m.bogucki@netigen.pl>
2019-07-04 14:24:40 +0200

    Correct errors

--- library/src/main/java/pl/netigen/premium/Premium.java
+++ library/src/main/java/pl/netigen/premium/Premium.java
@@ -1,5 +1,6 @@
 package pl.netigen.premium;
 
+import android.view.Display;
 import android.view.View;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import androidx.annotation.StringRes;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class PremiumBuilderNetigenApi {
+public class Premium {
 
     int bgImage;
     int titleImage;
@@ -18,13 +19,14 @@ public class PremiumBuilderNetigenApi {
     int imageButtonBuy;
     int premiumOptionsTitle;
     int textBuyButton;
+    Display display;
     String price;
     List<PremiumItemNetigenApi> premiumItemNetigenApis;
     View.OnClickListener clickBuyPremium;
 
-    private PremiumBuilderNetigenApi(int bgImage, int titleImage, int bgPremiumItem, int imageButtonBuy,
-                                     int premiumOptionsTitle, int textBuyButton, String price, List<PremiumItemNetigenApi> premiumItemNetigenApis,
-                                     View.OnClickListener clickBuyPremium) {
+    private Premium(int bgImage, int titleImage, int bgPremiumItem, int imageButtonBuy,
+                    int premiumOptionsTitle, int textBuyButton, String price, List<PremiumItemNetigenApi> premiumItemNetigenApis,
+                    View.OnClickListener clickBuyPremium,Display display) {
         this.bgImage = bgImage;
         this.titleImage = titleImage;
         this.bgPremiumItem = bgPremiumItem;
@@ -34,11 +36,12 @@ public class PremiumBuilderNetigenApi {
         this.price = price;
         this.premiumItemNetigenApis = premiumItemNetigenApis;
         this.clickBuyPremium = clickBuyPremium;
+        this.display = display;
     }
 
     public void openPremiumFragment(@NonNull FragmentManager fragmentManager,
                                     int frameId, String profile) {
-        testValues();
+
         PremiumFragmentNetigenApi premiumFragmentNetigenApi = PremiumFragmentNetigenApi.newInstance(this);
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(frameId, premiumFragmentNetigenApi, profile);
@@ -46,23 +49,6 @@ public class PremiumBuilderNetigenApi {
         transaction.commit();
     }
 
-    private void testValues() {
-        if (bgImage == 0) {
-            throw new IllegalArgumentException("You must add bgImage");
-        }
-        if (premiumOptionsTitle == 0) {
-            throw new IllegalArgumentException("You must add premiumOptionsTitle");
-        }
-        if (textBuyButton == 0) {
-            throw new IllegalArgumentException("You must add textBuyButton");
-        }
-        if (price == null) {
-            throw new IllegalArgumentException("You must add price");
-        }
-        if (premiumItemNetigenApis == null) {
-            throw new IllegalArgumentException("You must add premiumItemNetigenApis");
-        }
-    }
 
     public static class Builder {
         @DrawableRes
@@ -77,16 +63,18 @@ public class PremiumBuilderNetigenApi {
         int premiumOptionsTitle;
         @StringRes
         int textBuyButton;
+        Display display;
         String price;
         List<PremiumItemNetigenApi> premiumItemNetigenApis;
         View.OnClickListener clickBuyPremium;
 
-        public Builder() {
-        }
-
-        public Builder setBgImage(int bgImage) {
+        public Builder(int bgImage, int premiumOptionsTitle, int textBuyButton, String price, List<PremiumItemNetigenApi> premiumItemNetigenApis, Display display) {
             this.bgImage = bgImage;
-            return this;
+            this.premiumOptionsTitle = premiumOptionsTitle;
+            this.textBuyButton = textBuyButton;
+            this.price = price;
+            this.premiumItemNetigenApis = premiumItemNetigenApis;
+            this.display = display;
         }
 
         public Builder setTitleImage(int titleImage) {
@@ -104,34 +92,14 @@ public class PremiumBuilderNetigenApi {
             return this;
         }
 
-        public Builder setPremiumOptionsTitle(int premiumOptionsTitle) {
-            this.premiumOptionsTitle = premiumOptionsTitle;
-            return this;
-        }
-
-        public Builder setTextBuyButton(int textBuyButton) {
-            this.textBuyButton = textBuyButton;
-            return this;
-        }
-
-        public Builder setPremiumItemListNetigenApis(List<PremiumItemNetigenApi> premiumItemNetigenApis) {
-            this.premiumItemNetigenApis = premiumItemNetigenApis;
-            return this;
-        }
-
         public Builder setClickBuyPremium(View.OnClickListener clickBuyPremium) {
             this.clickBuyPremium = clickBuyPremium;
             return this;
         }
 
-        public Builder setPrice(String price) {
-            this.price = price;
-            return this;
-        }
-
-        public PremiumBuilderNetigenApi createObject() {
-            return new PremiumBuilderNetigenApi(bgImage, titleImage, bgPremiumItem, imageButtonBuy,
-                    premiumOptionsTitle, textBuyButton, price, premiumItemNetigenApis, clickBuyPremium);
+        public Premium createObject() {
+            return new Premium(bgImage, titleImage, bgPremiumItem, imageButtonBuy,
+                    premiumOptionsTitle, textBuyButton, price, premiumItemNetigenApis, clickBuyPremium,display);
         }
     }
 }
--- library/src/main/java/pl/netigen/premium/PremiumFragmentNetigenApi.java
+++ library/src/main/java/pl/netigen/premium/PremiumFragmentNetigenApi.java
@@ -1,6 +1,7 @@
 package pl.netigen.premium;
 
 
+import android.content.Context;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,16 +12,15 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-
 import java.util.List;
 
+import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.appcompat.widget.AppCompatTextView;
-import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import pl.netigen.netigenapi.R;
 
 
-public class PremiumFragmentNetigenApi extends Fragment {
+public class PremiumFragmentNetigenApi extends AppCompatDialogFragment {
 
 
     private int bgImage;
@@ -29,13 +29,14 @@ public class PremiumFragmentNetigenApi extends Fragment {
     private int imageButtonBuy;
     private int premiumOptionsTitle;
     private int textBuyButton;
+    private Point point;
 
     private List<PremiumItemNetigenApi> premiumItemNetigenApis;
     private CarouselEffectTransformer transformer;
     private View.OnClickListener clickBuyPremium;
 
 
-    private PremiumBuilderNetigenApi premiumBuilder;
+    private Premium premiumBuilder;
     private ViewPager viewPager;
     private ImageView backgroundPremium;
     private ImageView premiumTitleImage;
@@ -45,13 +46,13 @@ public class PremiumFragmentNetigenApi extends Fragment {
     private int pageMargin;
     private int viewPagerPadding;
 
-    public static PremiumFragmentNetigenApi newInstance(PremiumBuilderNetigenApi premiumBuilder) {
+    public static PremiumFragmentNetigenApi newInstance(Premium premiumBuilder) {
         PremiumFragmentNetigenApi fragment = new PremiumFragmentNetigenApi();
         fragment.setArguments(premiumBuilder);
         return fragment;
     }
 
-    private void setArguments(PremiumBuilderNetigenApi premiumBuilder) {
+    private void setArguments(Premium premiumBuilder) {
         this.premiumBuilder = premiumBuilder;
     }
 
@@ -59,42 +60,51 @@ public class PremiumFragmentNetigenApi extends Fragment {
     public PremiumFragmentNetigenApi() {
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View inflate = inflater.inflate(R.layout.fragment_premium_netigen_api, container, false);
+        View view = inflater.inflate(R.layout.fragment_premium_netigen_api, container, false);
+
         initValues();
-        bindView(inflate);
+        bindView(view);
         addImageAndText();
         calculationsValues();
         createViewPager();
-        return inflate;
+        return view;
     }
 
     private void initValues() {
-        bgImage = premiumBuilder.bgImage;
-        titleImage = premiumBuilder.titleImage;
-        bgPremiumItem = premiumBuilder.bgPremiumItem;
-        imageButtonBuy = premiumBuilder.imageButtonBuy;
-        premiumOptionsTitle = premiumBuilder.premiumOptionsTitle;
-        textBuyButton = premiumBuilder.textBuyButton;
-        premiumItemNetigenApis = premiumBuilder.premiumItemNetigenApis;
-        clickBuyPremium = premiumBuilder.clickBuyPremium;
+        point = new Point();
+        if(premiumBuilder != null) {
+            bgImage = premiumBuilder.bgImage;
+            titleImage = premiumBuilder.titleImage;
+            bgPremiumItem = premiumBuilder.bgPremiumItem;
+            imageButtonBuy = premiumBuilder.imageButtonBuy;
+            premiumOptionsTitle = premiumBuilder.premiumOptionsTitle;
+            textBuyButton = premiumBuilder.textBuyButton;
+            premiumItemNetigenApis = premiumBuilder.premiumItemNetigenApis;
+            clickBuyPremium = premiumBuilder.clickBuyPremium;
+            premiumBuilder.display.getSize(point);
+        } else {
+            dismiss();
+        }
+
     }
 
     private void calculationsValues() {
         float density = getResources().getDisplayMetrics().density;
-        int widthScale = getResources().getInteger(R.integer.widthScale);
-        int marginScale = getResources().getInteger(R.integer.marginScale);
+        int widthScale = getResources().getInteger(R.integer.widthScalePremiumNetigenApi);
+        int marginScale = getResources().getInteger(R.integer.marginScalePremiumNetigenApi);
         int partialWidth = (int) (widthScale * density); // 16dp
         pageMargin = (int) (marginScale * density); // 8dp
         viewPagerPadding = partialWidth + pageMargin;
-        Point screen = new Point();
-        getActivity().getWindowManager().getDefaultDisplay().getSize(screen);
-        float startOffset = (float) (viewPagerPadding) / (float) (screen.x - 2 * viewPagerPadding);
+        float startOffset = (float) (viewPagerPadding) / (float) (point.x - 2 * viewPagerPadding);
         transformer = new CarouselEffectTransformer(0, 0, 0.5f, startOffset);
-
     }
 
     private void createViewPager() {
@@ -103,18 +113,6 @@ public class PremiumFragmentNetigenApi extends Fragment {
         viewPager.setPageTransformer(false, transformer);
         PremiumPageAdapterNetigenApi adapter = new PremiumPageAdapterNetigenApi(premiumItemNetigenApis, bgPremiumItem);
         viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {/*empty*/}
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {/*empty*/}
-        });
     }
 
     private void bindView(View inflate) {
