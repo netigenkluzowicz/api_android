175bb500de29f160dab51448c242f474311b568b
Micpol <michal.polewczak@gmail.com>
2019-09-20 11:07:45 +0200

    Refactors rodo package name to gdpr

---
+++ library/src/main/java/pl/netigen/gdpr/DialogFragmentExtensions.kt
@@ -0,0 +1,29 @@
+package pl.netigen.gdpr
+
+import android.graphics.Point
+import android.view.Display
+import android.view.Gravity
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+fun DialogFragment.setDialogSize(widthMultiplier: Double, heightMultiplier: Double) {
+    val window = dialog?.window
+    val size = Point()
+    val display: Display
+    if (window != null) {
+        display = window.windowManager.defaultDisplay
+        display.getSize(size)
+        val maxWidth = size.x
+        val maxHeight = size.y
+        if (heightMultiplier == 0.0 && widthMultiplier == 0.0) {
+            window.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        } else if (widthMultiplier != 0.0 && heightMultiplier != 0.0) {
+            window.setLayout((maxWidth * widthMultiplier).toInt(), (maxHeight * heightMultiplier).toInt())
+        } else if (widthMultiplier != 0.0) {
+            window.setLayout((maxWidth * widthMultiplier).toInt(), ViewGroup.LayoutParams.WRAP_CONTENT)
+        } else if (heightMultiplier != 0.0) {
+            window.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, (maxHeight * heightMultiplier).toInt())
+        }
+        window.setGravity(Gravity.CENTER)
+    }
+}
