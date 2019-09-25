61f25e5f6d28d268f219243e241f1b64417ebf13
Micpol <michal.polewczak@gmail.com>
2019-09-25 11:00:00 +0200

    Adds paymentManager and methods to react on noAds state change

--- core/src/main/java/pl/netigen/core/netigenapi/NetigenMainActivity.kt
+++ core/src/main/java/pl/netigen/core/netigenapi/NetigenMainActivity.kt
@@ -13,16 +13,72 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
     lateinit var paymentManager: IPaymentManager
 
     open lateinit var viewModel: ViewModel
+    lateinit var paymentManager: IPaymentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getContentViewID())
         adsManager = AdsManager(viewModel, this)
-//        paymentManager = PaymentManager.createIPaymentManager(this)
-//        paymentManager.isItemPurchased(viewModel.noAdsSku, this)
+        initPayments()
     }
 
     abstract fun getContentViewID(): Int
+
+    private fun initPayments() {
+        paymentManager = PaymentManager.createIPaymentManager(this)
+        paymentManager.isItemPurchased(viewModel.noAdsSku, this)
+    }
+    @CallSuper
+    override fun onItemNotBought(sku: String?) {
+        if (!sku.isNullOrEmpty() && sku == viewModel.noAdsSku) {
+            showAds()
+            showBanner()
+            adsManager?.loadInterstitialIfPossible()
+        }
+    }
+
+    abstract fun showAds()
+
+    private fun showBanner() {
+        bannerRelativeLayout = getBannerRelativeLayout()
+        val layoutParams = bannerRelativeLayout?.layoutParams
+        val bannerHeightPixels = AdSize.SMART_BANNER.getHeightInPixels(this)
+        layoutParams?.width = RelativeLayout.LayoutParams.MATCH_PARENT
+        layoutParams?.height = bannerHeightPixels
+        bannerRelativeLayout?.setGravity(Gravity.TOP)
+        bannerRelativeLayout?.setLayoutParams(layoutParams)
+    }
+
+    @CallSuper
+    override fun onPaymentsError(errorMsg: String?) {
+        Toast.makeText(this, this.getString(R.string.payment_error) + " $errorMsg", Toast.LENGTH_LONG)
+                .show()
+    }
+
+    @CallSuper
+    override fun onItemBought(sku: String?) {
+        if (!sku.isNullOrEmpty() && sku == viewModel.noAdsSku) {
+            hideAds()
+            viewModel.isNoAdsBought = true
+            hideBanner()
+        }
+    }
+
+    abstract fun hideAds()
+
+    private fun hideBanner() {
+        bannerRelativeLayout = getBannerRelativeLayout()
+        val layoutParams = bannerRelativeLayout?.layoutParams
+        layoutParams?.width = RelativeLayout.LayoutParams.MATCH_PARENT
+        layoutParams?.height = 1
+        bannerRelativeLayout?.apply {
+            gravity = Gravity.TOP
+        }
+        bannerRelativeLayout?.layoutParams = layoutParams
+    }
+
+    abstract fun getBannerRelativeLayout(): RelativeLayout
+
 
     override fun onResume() {
         super.onResume()
