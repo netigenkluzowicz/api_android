b9443d2233424d41df76268fe2e9b05e038e0356
Micpol <michal.polewczak@gmail.com>
2019-09-30 10:03:39 +0200

    Implements basic onAttach

--- core/src/main/java/pl/netigen/core/netigenapi/NetigenFragment.kt
+++ core/src/main/java/pl/netigen/core/netigenapi/NetigenFragment.kt
@@ -1,4 +1,17 @@
 package pl.netigen.core.netigenapi
 
-class NetigenFragment {
+import android.app.Activity
+import android.content.Context
+import androidx.fragment.app.Fragment
+
+abstract class NetigenFragment : Fragment() {
+
+    private lateinit var netigenMainActivity: NetigenMainActivity<NetigenViewModel>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is Activity) {
+            netigenMainActivity = context as NetigenMainActivity<NetigenViewModel>
+        }
+    }
 }
