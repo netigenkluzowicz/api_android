b9d27e1ce02faf053fc741e520e7dff1e74d1981
Micpol <michal.polewczak@gmail.com>
2019-09-30 10:04:46 +0200

    Adds methods to show/hide bannerAd divider, removes splash screen logic

--- core/src/main/java/pl/netigen/core/netigenapi/NetigenMainActivity.kt
+++ core/src/main/java/pl/netigen/core/netigenapi/NetigenMainActivity.kt
@@ -2,10 +2,12 @@ package pl.netigen.core.netigenapi
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.google.android.gms.ads.AdSize
 import pl.netigen.billingandroid.IPaymentManager
 import pl.netigen.billingandroid.PaymentManager
@@ -18,7 +20,7 @@ import java.lang.NullPointerException
 abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActivity(), PurchaseListener {
 
     open lateinit var viewModel: ViewModel
-    lateinit var paymentManager: IPaymentManager
+    private lateinit var paymentManager: IPaymentManager
     private var bannerRelativeLayout: RelativeLayout? = null
     var adsManager: AdsManager? = null
 
@@ -26,7 +28,6 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
         super.onCreate(savedInstanceState)
         setContentView(getContentViewID())
         initPayments()
-        initAdsManager()
     }
 
     abstract fun getContentViewID(): Int
@@ -36,38 +37,51 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
         paymentManager.isItemPurchased(viewModel.noAdsSku, this)
     }
 
-    private fun initAdsManager() {
+    fun initAdsManager() {
         if (!viewModel.isNoAdsBought) {
             adsManager = AdsManager(viewModel, this)
             if (viewModel.config.rewardedAdId != null) {
                 if (prepareRewardedAdsListener() == null)
                     throw NullPointerException("Trying to load rewardedAds without a callback, prepareRewardedAdsListener should be overriden")
+                adsManager?.initRewardedVideoAd(prepareRewardedAdsListener()!!)
                 adsManager?.loadRewardedVideo()
             }
         }
     }
+
+    abstract fun showHomeFragment()
 
     open fun prepareRewardedAdsListener(): RewardsListener? = null
 
     @CallSuper
     override fun onItemNotBought(sku: String?) {
         if (!sku.isNullOrEmpty() && sku == viewModel.noAdsSku) {
-            showAds()
-            showBanner()
             adsManager?.loadInterstitialIfPossible()
         }
     }
 
     abstract fun showAds()
 
-    private fun showBanner() {
+    private fun onNoAdsNotBought() {
+        initAdsManager()
+    }
+
+    fun showBanner() {
         bannerRelativeLayout = getBannerRelativeLayout()
         val layoutParams = bannerRelativeLayout?.layoutParams
         val bannerHeightPixels = AdSize.SMART_BANNER.getHeightInPixels(this)
         layoutParams?.width = RelativeLayout.LayoutParams.MATCH_PARENT
         layoutParams?.height = bannerHeightPixels
-        bannerRelativeLayout?.setGravity(Gravity.TOP)
-        bannerRelativeLayout?.setLayoutParams(layoutParams)
+        bannerRelativeLayout?.gravity = Gravity.TOP
+        bannerRelativeLayout?.layoutParams = layoutParams
+        getBannerDividerView().visibility = View.VISIBLE
+    }
+
+    private fun onNoAdsBought() {
+        hideBanner()
+        hideAds()
+        showHomeFragment()
+        viewModel.isNoAdsBought = true
     }
 
     @CallSuper
@@ -79,15 +93,13 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
     @CallSuper
     override fun onItemBought(sku: String?) {
         if (!sku.isNullOrEmpty() && sku == viewModel.noAdsSku) {
-            hideAds()
-            viewModel.isNoAdsBought = true
-            hideBanner()
+            onNoAdsBought()
         }
     }
 
     abstract fun hideAds()
 
-    private fun hideBanner() {
+    fun hideBanner() {
         bannerRelativeLayout = getBannerRelativeLayout()
         val layoutParams = bannerRelativeLayout?.layoutParams
         layoutParams?.width = RelativeLayout.LayoutParams.MATCH_PARENT
@@ -96,10 +108,11 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
             gravity = Gravity.TOP
         }
         bannerRelativeLayout?.layoutParams = layoutParams
+        getBannerDividerView().visibility = View.GONE
     }
 
     abstract fun getBannerRelativeLayout(): RelativeLayout
-
+    abstract fun getBannerDividerView(): View
 
     override fun onResume() {
         super.onResume()
@@ -109,7 +122,6 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
             adsManager?.onResume(getBannerRelativeLayout())
         }
     }
-
 
     override fun onPause() {
         super.onPause()
@@ -124,4 +136,5 @@ abstract class NetigenMainActivity<ViewModel : NetigenViewModel> : AppCompatActi
             adsManager?.onDestroy()
         }
     }
+
 }
