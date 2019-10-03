d4f4e10f38125be6f0980bb255e622874998ec38
Micpol <michal.polewczak@gmail.com>
2019-10-03 11:21:09 +0200

    Converts Config and builder to Kotlin

--- core/src/main/java/pl/netigen/core/config/Config.kt
+++ core/src/main/java/pl/netigen/core/config/Config.kt
@@ -1,40 +1,26 @@
 package pl.netigen.core.config
 
-import pl.netigen.core.utils.SingletonHolder
-
-class Config private constructor(val inDebugMode: Boolean, val isSamsung: Boolean) {
-
-    private val testRewardedAdId = "ca-app-pub-3940256099942544/5224354917"
-    private val testBannerAdId = "ca-app-pub-3940256099942544/6300978111"
-    private val testInterstitialAdId = "ca-app-pub-3940256099942544/1033173712"
-
-    var isNoAdsBought: Boolean = false
-    var isNoAdsPaymentAvailable: Boolean = true
-    var isDesignedForFamily: Boolean = false
-    var rewardedAdId: String? = null
-    var adMobAppId: String? = null
-    var publishersId = arrayOf("pub-4699516034931013")
-    var isMultiScreen = false
-    var bannerAdId: String = testBannerAdId
-    var interstitalAdId: String = testInterstitialAdId
-
-    lateinit var testDevices: List<String>
+class Config constructor(val inDebugMode: Boolean, val isSamsung: Boolean,
+                         var isNoAdsBought: Boolean = false, var isNoAdsPaymentAvailable: Boolean = true,
+                         var isDesignedForFamily: Boolean = false, var rewardedAdId: String? = null,
+                         var adMobAppId: String? = null, var publishersId: Array<String> = arrayOf("pub-4699516034931013"),
+                         var isMultiScreen: Boolean = false, var bannerAdId: String = testBannerAdId,
+                         var interstitialAdId: String = testInterstitialAdId, var testDevices: ArrayList<String> = ArrayList()) {
 
     init {
         if (inDebugMode) {
             bannerAdId = testBannerAdId
-            interstitalAdId = testInterstitialAdId
+            interstitialAdId = testInterstitialAdId
             if (rewardedAdId != null) {
                 rewardedAdId = testRewardedAdId
             }
         }
-        testDevices = ArrayList<String>()
     }
 
-    companion object : SingletonHolder<Config, Boolean, Boolean>(::Config) {
-
+    companion object {
+        const val testRewardedAdId = "ca-app-pub-3940256099942544/5224354917"
+        val testBannerAdId = "ca-app-pub-3940256099942544/6300978111"
+        val testInterstitialAdId = "ca-app-pub-3940256099942544/1033173712"
     }
 
 }
-
-
