6afd7236e5cded422fa6ff3b740d1b335efec495
Kuba <jakub.rogala.95@gmail.com>
2019-12-19 09:16:40 +0100

    Add method for get isRewardAdLoaded()

--- core/src/main/java/pl/netigen/core/ads/RewardedAd.kt
+++ core/src/main/java/pl/netigen/core/ads/RewardedAd.kt
@@ -144,6 +144,8 @@ class RewardedAd(var viewModel: NetigenViewModel, val activity: AppCompatActivit
     fun removeListener(listener: RewardsListener) {
         rewardsListeners.remove(listener)
     }
+
+    fun isRewardedVideoAdLoaded(): Boolean = rewardedVideoAd.isLoaded
 }
 
 private const val TAG = "RewardedAd"
