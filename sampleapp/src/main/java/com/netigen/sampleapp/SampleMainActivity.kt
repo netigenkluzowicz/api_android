75ede766511ce08775a2fa5a456508147894f3e2
WrobelKrakow <m.wroblewski@netigen.pl>
2019-12-31 12:07:36 +0100

    Added sample app module

---
+++ sampleapp/src/main/java/com/netigen/sampleapp/SampleMainActivity.kt
@@ -0,0 +1,12 @@
+package com.netigen.sampleapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SampleMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_main)
+    }
+}
