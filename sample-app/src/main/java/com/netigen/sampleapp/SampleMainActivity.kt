37f0b26b79ca46655714148dfa44fc534442e2ea
WrobelKrakow <m.wroblewski@netigen.pl>
2020-01-08 09:11:10 +0100

    Created sample app

---
+++ sample-app/src/main/java/com/netigen/sampleapp/SampleMainActivity.kt
@@ -0,0 +1,53 @@
+package com.netigen.sampleapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import pl.netigen.core.network.NetworkStatus
+import pl.netigen.core.purchases.NoAdsNoAvailable
+import pl.netigen.core.splash.SplashVM
+import pl.netigen.coreapi.gdpr.AdConsentStatus
+import pl.netigen.coreapi.gdpr.CheckGDPRLocationStatus
+import pl.netigen.coreapi.gdpr.IGDPRConsent
+import pl.netigen.coreapi.splash.ISplashVM
+import pl.netigen.extensions.observe
+import pl.netigen.gms.ads.AdmobAds
+
+class SampleMainActivity : AppCompatActivity() {
+
+    private val gdprConsent = object : IGDPRConsent {
+        override val lastKnownAdConsentStatus: AdConsentStatus
+            get() = AdConsentStatus.PERSONALIZED_NON_UE
+
+        override fun requestGDPRLocation(gdprLocationStatusListener: (result: CheckGDPRLocationStatus) -> Unit) {}
+
+        override fun saveAdConsentStatus(adConsentStatus: AdConsentStatus) {}
+
+        override fun cancelRequest() {}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_main)
+
+        val ads = AdmobAds(
+            activity = this,
+            bannerAdId = "",
+            interstitialAdId = "",
+            bannerRelativeLayout = findViewById(R.id.adsLayout),
+            isInDebugMode = true
+        )
+
+        val splashVM : ISplashVM = SplashVM(
+            gdprConsent = gdprConsent,
+            ads = ads,
+            networkStatus = NetworkStatus(this),
+            noAdsPurchases = NoAdsNoAvailable()
+        )
+        splashVM.splashState.observe(this) {
+            Log.d("wrobel", "splashState.observe: $it")
+        }
+        splashVM.onStart()
+
+    }
+}
