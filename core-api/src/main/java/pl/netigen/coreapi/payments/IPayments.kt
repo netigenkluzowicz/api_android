75eb3826b43a51d7a3cc3774a4b46ffb05d906ed
Micpol <michal.polewczak@gmail.com>
2020-01-14 14:17:04 +0100

    Adds proper makeNoAdsPayment method

--- core-api/src/main/java/pl/netigen/coreapi/payments/IPayments.kt
+++ core-api/src/main/java/pl/netigen/coreapi/payments/IPayments.kt
@@ -1,12 +1,14 @@
 package pl.netigen.coreapi.payments
 
+import android.app.Activity
 import androidx.lifecycle.LiveData
 import pl.netigen.coreapi.payments.model.NetigenSkuDetails
 
-interface IPayments: INoAds {
+interface IPayments : INoAds {
     val inAppSkuDetails: LiveData<List<NetigenSkuDetails>>
     val subsSkuDetails: LiveData<List<NetigenSkuDetails>>
 
-    fun makePurchase()
+    fun makePurchase(activity: Activity, netigenSkuDetails: NetigenSkuDetails)
+
     fun consumeItem()
 }
