20cc76bec86d017c14f05792b66f2ef95d915bac
Micpol <michal.polewczak@gmail.com>
2020-01-17 12:40:07 +0100

    Updates splash VM

--- core-api/src/main/java/pl/netigen/coreapi/splash/SplashVM.kt
+++ core-api/src/main/java/pl/netigen/coreapi/splash/SplashVM.kt
@@ -7,6 +7,8 @@ import pl.netigen.coreapi.payments.INoAds
 abstract class SplashVM : ViewModel(), INoAds {
     abstract val splashState: LiveData<SplashState>
     abstract val isFirstLaunch: LiveData<Boolean>
-    abstract fun setPersonalizedAds(personalizedAdsApproved: Boolean)
     abstract val isNoAdsAvailable: Boolean
+    
+    abstract fun setPersonalizedAds(personalizedAdsApproved: Boolean)
+    abstract fun onStart()
 }
