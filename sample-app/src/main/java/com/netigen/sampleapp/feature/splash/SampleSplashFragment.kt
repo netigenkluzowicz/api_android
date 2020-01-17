cf3d2ac84baa168618e801242861698bf6f7bc64
Micpol <michal.polewczak@gmail.com>
2020-01-17 12:44:08 +0100

    Adds splashFragment to sample app.

---
+++ sample-app/src/main/java/com/netigen/sampleapp/feature/splash/SampleSplashFragment.kt
@@ -0,0 +1,40 @@
+package com.netigen.sampleapp.feature.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.safeNavigate
+import com.netigen.sampleapp.R
+import com.netigen.sampleapp.feature.home.SampleMainActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import pl.netigen.core.splash.SplashFragment
+import pl.netigen.coreapi.splash.SplashVM
+import timber.log.Timber
+
+@ExperimentalCoroutinesApi
+class SampleSplashFragment : SplashFragment() {
+
+    override val viewModel: SplashVM by lazy {
+        (activity!! as SampleMainActivity).splashVM
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onFinished() {
+        super.onFinished()
+        Timber.d("")
+        findNavController().safeNavigate(
+            R.id.action_splashFragment_to_homeFragment, null, NavOptions.Builder()
+                .setPopUpTo(
+                    R.id.splashFragment,
+                    true
+                )
+                .build()
+        )
+    }
+}
