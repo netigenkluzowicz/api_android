c8a2f373cbd8c9972b7328e7604e9ca3ec25009f
WrobelKrakow <m.wroblewski@netigen.pl>
2020-04-25 16:26:36 +0200

    Fixed test reward in gms sample-app

--- sample-app/src/gms/java/pl/netigen/sampleapp/flavour/FlavoursConst.kt
+++ sample-app/src/gms/java/pl/netigen/sampleapp/flavour/FlavoursConst.kt
@@ -5,13 +5,14 @@ import pl.netigen.core.config.AppConfig
 import pl.netigen.core.main.CoreMainActivity
 import pl.netigen.coreapi.payments.IPayments
 import pl.netigen.gms.ads.AdMobAds
+import pl.netigen.gms.ads.AdMobAds.Companion.TEST_REWARDED_ID
 import pl.netigen.gms.gdpr.GDPRConsentImpl
 import pl.netigen.gms.payments.GMSPayments
 
 object FlavoursConst {
     const val bannerAdId: String = ""
     const val interstitialAdId: String = ""
-    const val rewardedAdId: String = ""
+    const val rewardedAdId: String = TEST_REWARDED_ID
 
     fun getPaymentsImpl(coreMainActivity: CoreMainActivity): IPayments = GMSPayments(coreMainActivity.application)
     fun getAdsImpl(coreMainActivity: CoreMainActivity, appConfig: AppConfig) = AdMobAds(coreMainActivity, appConfig)
