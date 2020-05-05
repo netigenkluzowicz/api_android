13526f6e805b83cef6ef3a1958d03d11a0798d92
WrobelKrakow <m.wroblewski@netigen.pl>
2020-05-05 17:55:33 +0200

    INoAds.kt documentation fixed

--- core-api/src/main/java/pl/netigen/coreapi/payments/INoAds.kt
+++ core-api/src/main/java/pl/netigen/coreapi/payments/INoAds.kt
@@ -18,8 +18,8 @@ interface INoAds {
      * Makes call to current payment implementation to launch billing flow for an no ads in-app purchase.
      * It will show the purchase screen to the user
      *
-     * @param activity - activity used to as context for launch
-     * @param noAdsString - optional parameter - no ads in-app purchase identifier, use only when it is more than one no ad purchase available
+     * @param activity activity used to as context for launch
+     * @param noAdsString optional parameter - no ads in-app purchase identifier, use only when it is more than one no ad purchase available
      */
     fun makeNoAdsPayment(activity: Activity, noAdsString: String = "${activity.packageName}.noads")
 }
