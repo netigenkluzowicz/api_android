14e6377c32af2073bb82524f9a4f1fb4f40a6680
katarzyna krzemień <katarzynaiwonakrzemien@gmail.com>
2020-10-28 10:33:17 +0100

    LocalBillingDb migraction

--- gms/src/main/java/pl/netigen/gms/payments/LocalBillingDb.kt
+++ gms/src/main/java/pl/netigen/gms/payments/LocalBillingDb.kt
@@ -21,12 +21,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import pl.netigen.coreapi.payments.model.NetigenSkuDetails
 
 @Database(
-    entities = [NetigenSkuDetails::class, CachedPurchase::class],
-    version = 1,
-    exportSchema = false
+        entities = [NetigenSkuDetails::class, CachedPurchase::class],
+        version = 2,
+        exportSchema = false
 )
 @TypeConverters(PurchaseTypeConverter::class)
 abstract class LocalBillingDb : RoomDatabase() {
@@ -39,16 +41,26 @@ abstract class LocalBillingDb : RoomDatabase() {
         private const val DATABASE_NAME = "gms_purchase_db"
 
         fun getInstance(context: Context): LocalBillingDb =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: buildDatabase(context.applicationContext).also {
-                    INSTANCE = it
+                INSTANCE ?: synchronized(this) {
+                    INSTANCE ?: buildDatabase(context.applicationContext).also {
+                        INSTANCE = it
+                    }
                 }
-            }
 
         private fun buildDatabase(appContext: Context): LocalBillingDb {
             return Room.databaseBuilder(appContext, LocalBillingDb::class.java, DATABASE_NAME)
-                .fallbackToDestructiveMigration()
-                .build()
+                    .addMigrations(MIGRATION_1_2)
+                    .fallbackToDestructiveMigration()
+                    .build()
         }
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE $NETIGEN_DETAILS_TABLE_NAME ADD COLUMN priceAmountMicros INTEGER DEFAULT 0")
+                database.execSQL("ALTER TABLE $NETIGEN_DETAILS_TABLE_NAME ADD COLUMN priceCurrencyCode TEXT DEFAULT ''")
+            }
+        }
+
+        private const val NETIGEN_DETAILS_TABLE_NAME = "NetigenSkuDetails"
     }
 }
