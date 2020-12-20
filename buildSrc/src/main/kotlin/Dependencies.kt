8b475bae35c9543cf09aad5a802764507622a420
WrobelKrakow <m.wroblewski@netigen.pl>
2020-12-20 12:13:55 +0100

    coroutines = "1.4.2"

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -19,7 +19,7 @@ object Versions {
     const val espressoCore = "3.2.0"
     const val billing = "3.0.2"
     const val navigation = "2.2.2"
-    const val coroutines = "1.3.9"
+    const val coroutines = "1.4.2"
     const val room = "2.2.5"
     const val timber = "4.7.1"
 }
