5ee32a4db92204f73037046da24f12e7bd82d4e5
wrobel <m.wroblewski@netigen.pl>
2021-08-12 09:06:58 +0200

    DEFAULT_SPLASH_MAX_LOAD_INTERSTITIAL_WAIT_TIME_MS = 8_000L

--- core-api/src/main/java/pl/netigen/coreapi/splash/ISplashConfig.kt
+++ core-api/src/main/java/pl/netigen/coreapi/splash/ISplashConfig.kt
@@ -13,12 +13,12 @@ interface ISplashConfig {
     /**
      * Max white time for load splash interstitial ad, it is start counting after consent operations are finished
      *
-     * Default value: [7 seconds][DEFAULT_SPLASH_MAX_LOAD_INTERSTITIAL_WAIT_TIME_MS]
+     * Default value: [8 seconds][DEFAULT_SPLASH_MAX_LOAD_INTERSTITIAL_WAIT_TIME_MS]
      */
     val maxInterstitialWaitTime: Long
 
 
     companion object {
-        const val DEFAULT_SPLASH_MAX_LOAD_INTERSTITIAL_WAIT_TIME_MS = 7_000L
+        const val DEFAULT_SPLASH_MAX_LOAD_INTERSTITIAL_WAIT_TIME_MS = 8_000L
     }
 }
