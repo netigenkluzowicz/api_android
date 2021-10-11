e6c25e7344fdd460726b6790000ce673f65589a9
wrobel <m.wroblewski@netigen.pl>
2021-10-11 14:03:29 +0200

    New dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,14 @@
 object Versions {
     const val legacySupportV13 = "1.0.0"
-    const val playServicesAds = "20.2.0"
-    const val constraintLayout = "1.1.3"
+    const val playServicesAds = "20.4.0"
+    const val constraintLayout = "2.1.1"
     const val material = "1.4.0"
     const val gson = "2.8.7"
     const val glide = "4.12.0"
-    const val appcompat = "1.2.0"
+    const val appcompat = "1.3.1"
     const val userMessagingPlatform = "1.0.0"
     const val coreKTX = "1.6.0"
-    const val kotlin = "1.5.21"
+    const val kotlin = "1.5.31"
     const val okhttp = "4.9.0"
     const val retrofit = "2.9.0"
     const val junit = "4.12"
@@ -19,7 +19,7 @@ object Versions {
     const val coroutines = "1.5.1"
     const val room = "2.3.0"
     const val timber = "4.7.1"
-    const val playCore = "1.9.0"
+    const val playCore = "1.10.2"
 }
 
 object Libraries {
