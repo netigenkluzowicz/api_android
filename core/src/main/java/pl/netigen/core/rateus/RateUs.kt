3c5e6ee5ec82bb2a2b1a9b2ece59ad26405f49c0
wrobel <m.wroblewski@netigen.pl>
2023-05-30 11:17:50 +0200

    Reset open timer on rateus/survey launched

--- core/src/main/java/pl/netigen/core/rateus/RateUs.kt
+++ core/src/main/java/pl/netigen/core/rateus/RateUs.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.play.core.review.ReviewManagerFactory
 import pl.netigen.core.main.CoreMainActivity
 import pl.netigen.core.utils.Utils
+import pl.netigen.coreapi.main.ICoreMainActivity
 import pl.netigen.coreapi.main.ICoreMainActivity.Companion.KEY_IS_RATE_US_OPEN
 import pl.netigen.coreapi.main.ICoreMainActivity.Companion.KEY_NUMBER_OF_OPENINGS
 import pl.netigen.coreapi.main.ICoreMainActivity.Companion.NUMBER_OF_CHECKS_BEFORE_SHOWING_RATE_US
@@ -40,6 +41,7 @@ class RateUs private constructor(
         if (shouldOpenRateUs()) {
             val counter = coreMainActivity.openingCounter + 1
             if (counter % numberOfChecksBeforeShowingDialog == numberOfChecksBeforeShowingDialog - 1) {
+                resetCounterTime()
                 openRateDialog()
                 return true
             }
@@ -53,11 +55,16 @@ class RateUs private constructor(
         request.addOnCompleteListener { task ->
             Timber.d("task = [$task]")
             if (task.isSuccessful) {
+                resetCounterTime()
                 Timber.d("Show New Rate Us")
                 val reviewInfo = task.result
                 manager.launchReviewFlow(coreMainActivity, reviewInfo)
             }
         }
+    }
+
+    private fun resetCounterTime() {
+        sharedPreferences.edit().putLong(ICoreMainActivity.KEY_LAST_LAUNCH_TIME_COUNTER, 0L).apply()
     }
 
     override fun clickYes() {
