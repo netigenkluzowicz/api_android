ff20675c858b8dcb8cfdf2544809a74d2d112508
wrobel <m.wroblewski@netigen.pl>
2023-09-20 12:00:37 +0200

    Donate views

--- core/src/main/java/pl/netigen/core/donate/Donate.kt
+++ core/src/main/java/pl/netigen/core/donate/Donate.kt
@@ -1,7 +1,10 @@
 package pl.netigen.core.donate
 
 import pl.netigen.core.main.CoreMainActivity
+import pl.netigen.core.utils.Utils
 import pl.netigen.coreapi.payments.model.NetigenSkuDetails
+import pl.netigen.coreapi.payments.model.PaymentEvent
+import pl.netigen.coreapi.payments.model.PaymentSuccess
 import timber.log.Timber
 
 class Donate {
@@ -13,19 +16,38 @@ class Donate {
             for ((productId, _, _, _, priceAmountMicros, priceCurrencyCode, _, _, _) in skuDetails) {
 
                 val priceText = String.format("%.2f", priceAmountMicros?.div(1000000.0) ?: 0).replace(",", ".") + " " + priceCurrencyCode
-                if (productId.contains(Regex("\\.donate\\d"))) {
-                    donates.add(DonateInfo(productId, priceText))
+                if (matchesProductId(productId)) {
+                    donates.add(DonateInfo(productId, priceText, productId.last().digitToInt()))
                 }
             }
             Timber.d("donates = [$donates]")
         }
     }
 
-    fun showDialog(coreMainActivity: CoreMainActivity) = DonateFragment.newInstance()
-        .show(coreMainActivity.supportFragmentManager, "DonateFragment")
 
-    data class DonateInfo(val productId: String, val priceText: String)
+    fun showDialog(mainActivity: CoreMainActivity) {
+        if (donates.isEmpty()) {
+            Utils.showShortToast(mainActivity, "Error - payments info unavailable")
+            return
+        }
+        DonateFragment.newInstance(donates)
+            .show(mainActivity.supportFragmentManager, "DonateFragment")
+    }
+
+    fun checkShowCongrats(mainActivity: CoreMainActivity, paymentEvent: PaymentEvent) {
+        Timber.d("paymentEvent = [$paymentEvent]")
+        if (paymentEvent is PaymentSuccess && matchesProductId(paymentEvent.sku)) {
+            DonateThanksFragment()
+                .show(mainActivity.supportFragmentManager, "DonateFragment")
+        }
+    }
+
+    data class DonateInfo(val productId: String, val priceText: String, val productIndex: Int)
 
     val donatesAvailable
         get() = donates.isNotEmpty()
+
+    companion object {
+        fun matchesProductId(productId: String) = productId.contains(Regex("\\.donate\\d"))
+    }
 }
