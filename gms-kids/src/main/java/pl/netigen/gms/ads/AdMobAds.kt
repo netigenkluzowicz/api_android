cfea708177141ca5d7d630449eee41afe0ac3e20
wrobel <m.wroblewski@netigen.pl>
2024-01-31 11:56:36 +0100

    Gms kids TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE

--- gms-kids/src/main/java/pl/netigen/gms/ads/AdMobAds.kt
+++ gms-kids/src/main/java/pl/netigen/gms/ads/AdMobAds.kt
@@ -71,8 +71,12 @@ class AdMobAds(
             adId = rewardedId,
             yandexAdId = if (adsConfig.inDebugMode) "demo-rewarded-yandex" else adsConfig.rewardedYandexAdId,
         )
-        val requestConfiguration = RequestConfiguration.Builder()
-            .setTestDeviceIds(adsConfig.testDevices)
+        val requestConfiguration = MobileAds.getRequestConfiguration()
+            .toBuilder()
+            .setTagForChildDirectedTreatment(
+                RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE,
+            )
+            .setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_G)
             .build()
         MobileAds.setRequestConfiguration(requestConfiguration)
     }
