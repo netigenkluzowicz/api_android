392521577c3246f8cf6a1dbdc0397e3778824654
wrobel <m.wroblewski@netigen.pl>
2024-02-16 11:13:50 +0100

    MoreApps updated

---
+++ core/src/main/java/pl/netigen/core/moreapps/AppInfo.java
@@ -0,0 +1,28 @@
+package pl.netigen.core.moreapps;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AppInfo {
+    public final boolean isAd;
+    @SerializedName("package_name")
+    public final String packageName;
+    @SerializedName("name")
+    public final String appName;
+    public final String iconLink;
+    public final String fullAdLink;
+    public final int version;
+
+    AppInfo(String packageName, String appName, String iconLink, String fullAdLink, boolean isAd, int version) {
+        this.packageName = packageName;
+        this.appName = appName;
+        this.iconLink = iconLink;
+        this.fullAdLink = fullAdLink;
+        this.isAd = isAd;
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "AppInfo(" + packageName + ";" + appName + ";" + iconLink + ";" + fullAdLink + ";" + isAd + ")";
+    }
+}
