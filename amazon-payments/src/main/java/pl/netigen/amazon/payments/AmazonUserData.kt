c68867a5accc2a8e7d34080b704f6ff3ba83caa4
wrobel <m.wroblewski@netigen.pl>
2024-06-19 09:11:56 +0200

    Remove amazon - fix for failed builds

--- amazon-payments/src/main/java/pl/netigen/amazon/payments/AmazonUserData.kt
+++
@@ -1,8 +0,0 @@
-package pl.netigen.amazon.payments
-
-data class AmazonUserData(
-    val userId: String = "",
-    val marketplace: String = "",
-) {
-    val isEmpty = userId.isEmpty() || marketplace.isEmpty()
-}
